d0e0a876cc15ea7b71b3fcf025a3a394d839d07b
Pratik Deoghare <pratik.deoghare@gmail.com>
2021-11-30 17:37:23 +0100

    add node-ip flag

--- cmd/benchmate/main.go
+++ cmd/benchmate/main.go
@@ -9,28 +9,27 @@ import (
 	"sync"
 )
 
-func RunClients() {
-	tpResult, err := throughput.NewThroughputMeter(throughput.DefaultOptions()).Client()
+func RunClients(tpOpt throughput.Options, latOpt latency.Options) {
+	tpResult, err := throughput.NewThroughputMeter(tpOpt).Client()
 	if err != nil {
 		log.Println(err)
 	}
 	fmt.Println(tpResult)
 
-	latResult, err := latency.NewLatencyMeter(latency.DefaultOptions()).Client()
+	latResult, err := latency.NewLatencyMeter(latOpt).Client()
 	if err != nil {
 		log.Println(err)
 	}
 	fmt.Println(latResult)
 }
 
-func RunServers() {
+func RunServers(tpOpt throughput.Options, latOpt latency.Options) {
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		opt := throughput.DefaultOptions()
-		log.Println("throughput server running with: %s", opt)
-		err := throughput.NewThroughputMeter(opt).Server()
+		log.Println("throughput server running with: ", tpOpt)
+		err := throughput.NewThroughputMeter(tpOpt).Server()
 		if err != nil {
 			log.Println("throughput server: ", err)
 		}
@@ -38,9 +37,8 @@ func RunServers() {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		opt := latency.DefaultOptions()
-		log.Println("latency server running with: %s", opt)
-		err := latency.NewLatencyMeter(opt).Server()
+		log.Println("latency server running with: ", latOpt)
+		err := latency.NewLatencyMeter(latOpt).Server()
 		if err != nil {
 			log.Println("latency server: ", err)
 		}
@@ -56,12 +54,19 @@ func main() {
 	log.Println("Running...")
 
 	var c bool
+	var nodeIP string
 	flag.BoolVar(&c, "c", false, "set the flag to run in client mode. Default is server mode. ")
+	flag.StringVar(&nodeIP, "node-ip", "", "IP address of benchmate server")
 	flag.Parse()
+	log.Println("node-ip", nodeIP)
+	tpOpt := throughput.DefaultOptions()
+	latOpt := latency.DefaultOptions()
 	if c {
-		RunClients()
+		tpOpt.TcpAddress = nodeIP + ":13500"
+		latOpt.TcpAddress = nodeIP + ":13501"
+		RunClients(tpOpt, latOpt)
 	} else {
-		RunServers()
+		RunServers(tpOpt, latOpt)
 	}
 	log.Println("Finished.")
 }
